b53acee76f2b2edde2061f87899fa9ec40b768c1
Niwin Kumar <iamniwin@gmail.com>
2025-01-08 17:28:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ Created: January 2025
 """
 
 # Configuration settings
-MONGO_URI = "mongodb://localhost:27017/"
+MONGO_URI = "mongodb+srv://iamniwin:njWZn3OydYNJlwCi@niwin-cluster.aystl.mongodb.net/?retryWrites=true&w=majority&appName=Niwin-Cluster"
 MONGO_DB = "twitter_data"
 MONGO_COLLECTION = "trending_topics"
 PROXY_LIST = ["http://niwinkumar:password7@open.proxymesh.com:31280"]
